14c2d47b48022d1dc08353ff028bad61e92b4c7b
AdelinaGjonaj <adelina.sinaj@gmail.com>
2022-11-09 18:43:42 +0100

    modified:   About us/About.html 	modified:   Clients/clients.css 	modified:   Clients/clients.html 	modified:   Clients/clients.js 	modified:   Contact Us/contact.css 	modified:   Contact Us/contact.html 	modified:   Home/home.html 	modified:   Login/login.css 	modified:   Login/login.html 	modified:   Login/login.js 	modified:   Service/service.css 	modified:   Service/service.html 	new file:   signup/signup.css 	new file:   signup/signup.html 	new file:   signup/signup.js

---
+++ signup/signup.js
@@ -0,0 +1,20 @@
+
+function validateSignupForm() {
+	let mail = document.getElementById("signEmail").value;
+	let name = document.getElementById("signName").value;
+	let password = document.getElementById("signPassword").value;
+
+	if (mail == "" || name == "" || password == "") {
+		document.getElementById("errorMsg").innerHTML = "Please fill the required fields"
+		return false;
+	}
+
+	else if (password.length < 8) {
+		document.getElementById("errorMsg").innerHTML = "Your password must include atleast 8 characters"
+		return false;
+	}
+	else {
+		alert("Successfully signed up");
+		return true;
+	}
+}
